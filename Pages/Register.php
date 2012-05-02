ad51114d64bc99562d93f631b5c493ac37db207a
Giorgos Mermigkas <gdmerm@gmail.com>
2012-05-02 21:46:35 +0300

    added querystring in data contract

--- Pages/Register.php
+++ Pages/Register.php
@@ -3,6 +3,7 @@ class Register
 {
   public function initialize() {
 	  //controller code here
+	  TMPLPageController::appendToView("queryString", $_GET);
 	  TMPLPageController::setTemplateFile("templates/register.php");
   }
 }
