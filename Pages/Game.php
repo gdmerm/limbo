2d79d39df8a3f6990ee8f71b42ff71051bae528f
Giorgos Mermigkas <gdmerm@gmail.com>
2012-05-06 12:56:38 +0300

    added product view with model data

--- Pages/Game.php
+++ Pages/Game.php
@@ -9,6 +9,22 @@ class Game extends LMBPageController
 		LoginController::getUserInfo();
 		TMPLPageController::appendToView("queryString", $_GET);
 		TMPLPageController::setTemplateFile("templates/game.php");
+
+		//sanitize game id from querystring
+		if (!isset($_GET["id"])) {
+			exit;
+		}
+		$productid = $_GET["id"];
+		$productid = (is_numeric($productid)) ? $productid : "";
+		if ($productid === '') {exit;}
+
+		//provide data to view
+		$prodController = new Products();
+		$prodController->setDBlink($db);
+		$product = $prodController->getSingleById($productid);
+
+		TMPLPageController::appendToView("product", $product->getFields());
+
 	}
 }
 
