63990a85a860832be33bc1c0d33253a323d0ef3f
Giorgos Mermigkas <gdmerm@gmail.com>
2012-06-10 15:27:20 +0300

    small fixes

--- templates/genre.php
+++ templates/genre.php
@@ -26,28 +26,9 @@
 
         <!--Right column-->
         <div class="rightcolumn">
-			<div class="day-offers">
-				<div class="topblock-genre">
-					<div class="offers">Today's Offer</div>
-				</div>
-				<div class="mainblock">
-					<div class="imageblock">
-						<?php foreach ($view->data->dayOffers->games as $index => $product): ?>
-						<div class="tinyimg<?php echo ($index > 0) ? "2" : "" ?>">
-							<a href="#"><img src="<?php echo $config["assets"] ?>/images/thumbs/<?php echo $product->productid ?>/tiny.jpg"/></a>
-						</div>
-						<?php endforeach ?>
-					</div>
 
-					<div class="offerinfo">
-						<p><?php echo $view->data->dayOffers->promoText->text ?></p>
-					</div>
-					<div class="btn-addtocart">
-						<a href="#">ADD TO CART</a>
-					</div>
-					<div class="clear"></div>
-				</div>
-			</div>
+			<?php include("components/today_offer.php") ?>
+
         </div>
         <!--End Right Column-->
 
